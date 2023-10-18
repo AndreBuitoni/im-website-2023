35d10cae79d573315556ff06fae4492487783221
André Buitoni <a.buitoni@gmail.com>
2023-10-18 20:36:22 -0300

    Criação do visual inicial página Aprendizagem

--- im-website/src/pages/Aprendizagem.tsx
+++ im-website/src/pages/Aprendizagem.tsx
@@ -1,8 +1,15 @@
-export function Aprendizagem () {
+export function Aprendizagem() {
   return (
-    <div className="bg-gray-200 p-5 w-full mx-auto">
-      <h1 className="text-black font-bold text-xl">Aprendizagem</h1>
-      <p className="text-black py-4">Nessa página de APRENDIZAGEM você encontrará muitos conteúdos legais para aprender inglês!</p>
-    </div>
-  )
+    <main className="max-w-screen-2xl mx-auto bg-gray-300 p-4">
+      {/*Esta DIV é o título da página*/}
+      <div className="bg-gray-50 rounded-md p-4 mb-4 w-full mx-auto">
+        <h1 className="text-black font-bold text-4xl">
+          Aprendizagem - Aprenda e Pratique
+        </h1>
+        <p className="text-black text-xl py-4">
+          Esta página será o local onde você vai acessar diferentes jogos e atividades para aprender e praticar inglês. Algumas atividades atualmente em desenvolvimento são: jogos da memória e ditados.
+        </p>
+      </div>
+    </main>
+  );
 }
