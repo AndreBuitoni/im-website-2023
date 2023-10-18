3cbd9584b68c9f72e1b8c952e09cbfd04c564a10
André Buitoni <a.buitoni@gmail.com>
2023-10-18 20:37:12 -0300

    Alterações na criação dinâmica dos textos

--- im-website/src/components/Leituras.tsx
+++ im-website/src/components/Leituras.tsx
@@ -1,11 +1,13 @@
 import { Card } from "@/components/ui/Card"
+import database from '../database.json' 
 
 export function Leituras () {
+
   return (
-    <main className="w-full p-4 bg-gray-300 flex flex-col min-h-screen">
+    <main className="w-full p-4 bg-gray-300 flex flex-col">
         <div className="bg-gray-50 rounded-md p-4 mb-4">
           <h1 className="text-4xl font-bold text-black">
-            Textos Para Aprender Inglês
+            Leituras - Textos Para Aprender Inglês
           </h1>
           <p className="p-2 text-black text-xl">
             Os textos abaixo estão separados por níveis básicos (A1 e A2) e
@@ -13,24 +15,36 @@ export function Leituras () {
           </p>
         </div>
         <div className="w-full grid grid-cols-2 gap-4">
+          {/* Estes são os 4 CARDS, separados por níveis de inglês, contendo os links para os textos */}
           <Card 
             title="A1 - Iniciante"
-            links={["Greetings and Introductions", "My Daily Routine"
-            , "Family Matters", "1"]}
+            links={titlesByLevel("A1", database.texts)}
           />
           <Card 
             title="A2 - Elementar"
-            links={["Exploring My Hobbies", "Traveling Abroad", "Ordering Food at a Restaurant"]}
+            links={titlesByLevel("A2", database.texts)}
           />
           <Card 
             title="B1 - Intermediário"
-            links={["At the Airport", "Shopping for Clothes", "Describing Your Home"]}
+            links={titlesByLevel("B1", database.texts)}
           />
           <Card 
             title="B2 - Intermediário Superior"
-            links={["Health and Fitness", "Weather Talk", "Talking About Work", "Planning a Weekend Getaway", "Celebrating Holidays"]}
+            links={titlesByLevel("B2", database.texts)}
           />
         </div>
       </main>
   )
+}
+
+interface TextType {
+  id: number
+  level: string
+  title: string
+  content: string
+}
+
+function titlesByLevel(level: string, texts: TextType[]) {
+  
+  return texts.filter(t => t.level == level).map(t => t.title)
 }
--- im-website/src/components/Text.tsx
+++ im-website/src/components/Text.tsx
@@ -12,19 +12,17 @@ export function Text() {
 
   return (
     <>
-      <h1>Welcome to</h1>
-      <h1>Page {title} </h1>
       <div className="flex gap-2">
-        <div className="bg-gray-200 py-6 px-12 w-8/12 rounded">
+        {/* Seção do Texto */}
+        <div className="bg-gray-200 py-6 px-12 w-7/12 rounded">
           <h1 className="text-slate-950 font-bold text-2xl">{textToDisplay.title}</h1>
-          <p className="text-slate-950 text-xl py-4">{textToDisplay.paragraph1}</p>
-          <p className="text-slate-950 text-xl py-4">{textToDisplay.paragraph2}</p>
-          <p className="text-slate-950 text-xl py-4">{textToDisplay.paragraph3}</p>
-          <p className="text-slate-950 text-xl py-4">{textToDisplay.paragraph4}</p>
-          <p className="text-slate-950 text-xl py-4">{textToDisplay.paragraph5}</p>
+          <div className="text-slate-950 text-l py-4" dangerouslySetInnerHTML={{ __html: textToDisplay.content as string}}></div>
         </div>
-        <div className="bg-gray-200 p-4 w-4/12 rounded">
+
+        {/* Seção do Glossário */}
+        <div className="bg-gray-200 p-4 w-5/12 rounded">
         <h1 className="text-slate-950 font-bold text-2xl">Glossary</h1>
+        <div className="text-slate-950 text-l py-4" dangerouslySetInnerHTML={{ __html: textToDisplay.glossary as string}}></div>
         </div>
       </div>
     </>
